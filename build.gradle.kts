837589d57e7062bdcd4adfec2e7825cb335af15f
Patryk Pacewicz <patrykpacewicz@gmail.com>
2020-08-21 22:06:41 +0200

    Add graal-native

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,19 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.3.3.RELEASE"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    id("com.palantir.graal") version "0.7.1"
     kotlin("jvm") version "1.3.72"
     kotlin("plugin.spring") version "1.3.72"
 }
 
 group = "ppac.scheduler"
 version = "0.0.1"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+graal {
+    mainClass("org.springframework.boot.loader.JarLauncher")
+    outputName("simple-scheduler-$version")
+}
 
 repositories {
     mavenCentral()
@@ -25,6 +31,6 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        jvmTarget = "1.8"
     }
 }
